965bbcbfc1d54a7692d5f2451e706e2c00699b3f
datewu <hihahajun@gmail.com>
2022-11-30 22:16:27 +0800

    add  log

--- cmd/api/middleware.go
+++ cmd/api/middleware.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"time"
 
+	"github.com/datewu/gtea/jsonlog"
 	"github.com/datewu/set-img/internal/auth"
 	"github.com/datewu/set-img/internal/author"
 )
@@ -13,9 +14,11 @@ func checkAuth(token string) (bool, error) {
 	defer cancel()
 	ok, err := auth.Valid(ctx, auth.GithubAuth, token)
 	if err != nil {
+		jsonlog.Err(err)
 		return false, err
 	}
 	if !ok {
+		jsonlog.Err(nil, map[string]any{"token": token})
 		return false, nil
 	}
 	return author.Can(token)
