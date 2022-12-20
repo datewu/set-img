a7b94b64591d17948f93fe6654479394ab498245
datewu <hihahajun@gmail.com>
2022-12-20 11:08:23 +0800

    use debug level

--- cmd/main.go
+++ cmd/main.go
@@ -25,16 +25,14 @@ func main() {
 	flag.IntVar(&port, "port", 8080, "API server port")
 	flag.StringVar(&env, "env", "development", "Environment (development|staging|production)")
 	flag.StringVar(&kubeconfig, "kubeconfig", "in-cluster", "path to kubernetes config file")
-
 	flag.Parse()
 
 	utils.PanicFn(initK8s)
-
 	cfg := &gtea.Config{
 		Port:     port,
 		Env:      env,
 		Metrics:  true,
-		LogLevel: jsonlog.LevelInfo,
+		LogLevel: jsonlog.LevelDebug,
 	}
 	ctx := context.Background()
 	app := gtea.NewApp(ctx, cfg)
