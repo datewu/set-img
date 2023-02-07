8c0e7fd1e2e3bd58bbfa25462b747d0a05c7628c
datewu <hihahajun@gmail.com>
2023-02-07 16:11:52 +0800

    update to go 1.20.0

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -5,11 +5,18 @@ import (
 
 	"github.com/datewu/gtea"
 	"github.com/datewu/gtea/handler"
+	"github.com/datewu/gtea/handler/static"
 	"github.com/datewu/gtea/router"
 )
 
 func New(app *gtea.App) http.Handler {
 	r := router.DefaultRoutesGroup()
+	fs := static.FS{
+		NoDir:   true,
+		TryFile: []string{},
+		Root:    "front/dist",
+	}
+	r.ServeFSWithGzip("/", fs)
 	addBusinessRoutes(app, r)
 	return r.Handler()
 }
@@ -17,8 +24,8 @@ func New(app *gtea.App) http.Handler {
 func addBusinessRoutes(app *gtea.App, r *router.RoutesGroup) {
 	th := &tokenHandler{app: app}
 	kh := &k8sHandler{app: app}
-	r.Get("/", showPath)
 	g := r.Group("/api/v1")
+	g.Get("/", showPath)
 	a := g.Group("/auth", handler.TokenMiddleware(checkAuth))
 
 	a.Get("/ping", th.authPing)
