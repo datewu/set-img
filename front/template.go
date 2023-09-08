b0c05d93753378ed399c9508b216a774473a62f3
datewu <>
2023-09-08 17:58:11 +0800

    use embed to fight gh-action

--- front/template.go
+++ front/template.go
@@ -1,7 +1,14 @@
 package front
 
-import "html/template"
+import (
+	"html/template"
+
+	_ "embed"
+)
+
+//go:embed index-layout.html
+var s string
 
 // IndexTpl for index-layout.html
 var IndexTpl = template.Must(template.New("index").
-	Delims("{i{", "}i}").ParseFiles("index-layout.html"))
+	Delims("{i{", "}i}").Parse(s))
